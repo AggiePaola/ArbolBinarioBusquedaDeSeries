c2aa7a86c6b292e85940b29f212b095ddf0cba92
tu-nombre-de-usuario. <aggiepaola@gmail.com>
2024-09-16 18:18:48 -0500

    comentario clase ArbolBinarioNumeros.java

--- ArbolBinarioNumeros.java
+++ ArbolBinarioNumeros.java
@@ -1,57 +1,49 @@
 package arbol;
 
 public class ArbolBinarioNumeros {
-    private NodoArbol inicial;
-
-    public ArbolBinarioNumeros() {
-        this.inicial = null;
+    private NodoArbol inicial;//nodo privado de instancia inicial = raíz del arbol binario
+    
+    public ArbolBinarioNumeros() { //constructor de la clase
+        this.inicial = null; //inicializamos atributo .inicial para crear nueva instancia de la clase actual
     }
-
-    public void insertar(int valor) {
-        if (this.inicial == null) {
-            this.inicial = new NodoArbol(valor);
-        } else {
-            this.inicial.insertar(valor);
+    public void insertar(int valor) {//método insertar
+        if (this.inicial == null) { //si el arbol esta vacío ->
+            this.inicial = new NodoArbol(valor);//creamos la raíz con el valor proporcionado
+        } else { //si no esta vacío
+            this.inicial.insertar(valor); //llamamos método insertar para agregar el valor
         }
     }
-
-    public void dispararPreorden() {
-        this.preorden(this.inicial);
+    public void dispararPreorden() { //inicializar recorrido preorden
+        this.preorden(this.inicial); //llamar método preorden
+    } 
+    private void preorden(NodoArbol nodo) { //método preorden
+        if (nodo == null) { //si es null termina la recursión
+            return;}
+        System.out.println(nodo.getValor());//raiz
+        preorden(nodo.getNodoIzq());//izquierda
+        preorden(nodo.getNodoDerecho());//derecha
     }
-
-    private void preorden(NodoArbol nodo) {
-        if (nodo == null) {
-            return;
-        }
-        System.out.println(nodo.getValor());
-        preorden(nodo.getNodoIzq());
-        preorden(nodo.getNodoDerecho());
-    }
-
-    public void dispararInorden() {
+    
+    public void dispararInorden() { //inicializar recorrido preorden inorden
         this.inorden(this.inicial);
     }
-
     private void inorden(NodoArbol nodo) {
-        if (nodo == null) {
-            return;
-        }
-        inorden(nodo.getNodoIzq());
-        System.out.println(nodo.getValor());
-        inorden(nodo.getNodoDerecho());
+        if (nodo == null) {//si es null termina la recursión
+            return;}
+        inorden(nodo.getNodoIzq());//izquierda
+        System.out.println(nodo.getValor());//raiz
+        inorden(nodo.getNodoDerecho());//derecho
     }
-
-    public void dispararPostorden() {
+    
+    public void dispararPostorden() { //inicializar recorrido preorden postorden
         this.postorden(this.inicial);
     }
-
     private void postorden(NodoArbol nodo) {
-        if (nodo == null) {
-            return;
-        }
-        postorden(nodo.getNodoIzq());
-        postorden(nodo.getNodoDerecho());
-        System.out.println(nodo.getValor());
+        if (nodo == null) {//si es null termina la recursión
+            return;}
+        postorden(nodo.getNodoIzq());//izquierda
+        postorden(nodo.getNodoDerecho());//derecha
+        System.out.println(nodo.getValor());//raiz
     }
 }
 
